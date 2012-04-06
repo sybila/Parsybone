b87c9c2a6a3a3cf9668a448957b5038aca989573
Adam Streck <adam.streck@gmail.com>
2012-04-06 19:21:31 +0200

    Header recreated.

--- parsing/argument_parser.hpp
+++ parsing/argument_parser.hpp
@@ -1,17 +1,9 @@
 /*
  * Copyright (C) 2012 - Adam Streck
- *
  * This file is part of ParSyBoNe (Parameter Synthetizer for Boolean Networks) verification tool
- *
- * ParSyBoNe is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * ParSyBoNe is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3.
  * ParSyBoNe is released without any warrany. See the GNU General Public License for more details. <http://www.gnu.org/licenses/>.
- *
- * This software has been created as a part of a research conducted in the Systems Biology Laboratory of Masaryk University Brno.
- * See http://sybila.fi.muni.cz/ .
+ * This software has been created as a part of a research conducted in the Systems Biology Laboratory of Masaryk University Brno. See http://sybila.fi.muni.cz/ .
  */
 
 #ifndef PARSYBONE_ARGUMENT_PARSER_INCLUDED
