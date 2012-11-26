c657f19cf63821745b45a6a546e96422df259351
Adam Streck <adam.streck@gmail.com>
2012-11-26 19:48:51 +0100

    SQLite added.

---
+++ synthesis/SQLAdapter.hpp
@@ -0,0 +1,10 @@
+#ifndef PARSYBONE_SQLADAPTER_INCLUDED
+#define PARSYBONE_SQLADAPTER_INCLUDED
+
+#include "../sqlite/sqlite3.h"
+
+class SQLAdapter {
+
+};
+
+#endif // PARSYBONE_SQLADAPTER_INCLUDED
