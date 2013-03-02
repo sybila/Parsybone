86eab3f9a6b8c457e14032874306465220b22e22
Adam Streck <adam.streck@gmail.com>
2013-03-02 16:12:12 +0100

    Formulae test moved to GTest environment.

--- testing/includes.hpp
+++ testing/includes.hpp
@@ -1,10 +1,6 @@
 #ifndef PARSYBONE_INCLUDES_INCLUDED
 #define PARSYBONE_INCLUDES_INCLUDED
 
-#include "../auxiliary/common_functions.hpp"
-#include "../auxiliary/data_types.hpp"
-#include "../parsing/formulae_parser.hpp"
-#include "../parsing/model_parser.hpp"
-#include "../parsing/model.hpp"
+
 
 #endif // PARSYBONE_INCLUDES_INCLUDED
